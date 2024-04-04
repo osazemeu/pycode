b2a56879e8bbceecf6698720639f20906fccb315
Osazeme Usen <osazeme.usen@casper.com>
2024-04-04 12:11:47 -0400

    move api key to env for security

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
+from dotenv import load_dotenv
 import argparse
+
+load_dotenv()
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--task", default="return a list of numbers")
 parser.add_argument("--language", default="python")
 args = parser.parse_args()
 
-# SECURE THIS KEY
-api_key = "<OPENAI API KEY>"
-
-llm = OpenAI(
-    openai_api_key = api_key
-)
+llm = OpenAI()
 
 code_prompt = PromptTemplate(
     template="Write a very short {language} function that will {task}",
