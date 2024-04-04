513fcaeb6a0ba1ab05d940aa7e774eee427afd4b
Osazeme Usen <osazeme.usen@casper.com>
2024-04-04 11:40:14 -0400

    basic prompt template using langchain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 
 # SECURE THIS KEY
 api_key = "<OPENAI API KEY>"
@@ -7,5 +9,18 @@ llm = OpenAI(
     openai_api_key = api_key
 )
 
-result = llm('Write a very very short poem')
+code_prompt = PromptTemplate(
+    template="Write a very short {language} function that will {task}",
+    input_variables=["language", "task"]
+)
+
+code_chain= LLMChain(
+    llm=llm,
+    prompt=code_prompt
+)
+
+result = code_chain({
+    "language": "python",
+    "task": "return a list of numbers"
+})
 print(result)
