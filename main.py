2e21d4fbe2383b82ec1df669a1de4d020b4135a6
Osazeme Usen <osazeme.usen@casper.com>
2024-04-05 00:30:45 -0400

    use sequentialchain from langchain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain, SequentialChain
 from dotenv import load_dotenv
 import argparse
 
@@ -18,12 +18,30 @@ code_prompt = PromptTemplate(
     input_variables=["language", "task"]
 )
 
-code_chain= LLMChain(
-    llm=llm,
-    prompt=code_prompt
+test_prompt = PromptTemplate(
+    input_variables=["language", "code"],
+    template="Write a {language} unit test for the following code:\n{code}"
 )
 
-result = code_chain({
+code_chain= LLMChain(
+    llm=llm,
+    prompt=code_prompt,
+    output_key="code"
+)
+
+test_chain = LLMChain(
+    llm=llm,
+    prompt=test_prompt,
+    output_key="test"
+)
+
+chain = SequentialChain(
+    chains=[code_chain, test_chain],
+    input_variables=["task", "language"],
+    output_variables=["test", "code"]
+)
+
+result = chain({
     "language": args.language,
     "task": args.task
 })
